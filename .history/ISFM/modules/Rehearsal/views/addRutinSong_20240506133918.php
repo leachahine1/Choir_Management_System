2fcc52430c9caca7fe5eac4b90ecfc518996c2fc
lea chahine <lea.chahine@lau.edu>
2024-05-06 14:21:13 +0300

    db fixes

---
+++ .history/ISFM/modules/Rehearsal/views/addRutinSong_20240506133918.php
@@ -0,0 +1,138 @@
+<!-- BEGIN CONTENT -->
+<div class="page-content-wrapper">
+    <div class="page-content">
+        <!-- BEGIN PAGE CONTENT-->
+        <?php
+        foreach ($rehearsalInfo as $row) {
+            $rehearsalID = $row['id'];
+            $rehearsalTitle = $row['rehearsal_title'];
+            $startDate = $row['start_date'];
+        }
+        ?>
+        <div class="row">
+            <div class="col-md-12 ">
+                <!-- BEGIN SAMPLE FORM PORTLET-->
+                <div class="portlet box purple ">
+                    <div class="portlet-title">
+                        <div class="caption">
+                            <i class="fa fa-bars"></i>  <?php echo lang('exa_mr_1') . ' ' . $rehearsalTitle . lang('exa_mr_1'); ?>
+                        </div>
+                        <div class="tools">
+                            <a href="" class="collapse">
+                            </a>
+                            <a href="" class="reload">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="portlet-body form">
+                        <?php
+                        $form_attributs = array('class' => 'form-horizontal', 'role' => 'form');
+                        echo form_open('rehearsal/completRehearsalRoutin', $form_attributs);
+                        ?>
+                        <div class="form-body">
+                            <?php
+                            if (!empty($successMessage)) {
+                                echo $successMessage;
+                            }
+                            ?> 
+                            <div class="alert alert-info">
+                                <div class="form-group">
+                                    <div id="div_scents">
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <input type="hidden" class="form-control" value="<?php echo $rehearsalID; ?>" name="rehearsalId">
+                                                <h3 class="arpl"><?php echo lang('exa_rehearsal'); ?> </h3>
+                                                <input type="hidden" class="form-control" name="rehearsalSunjectFild" value="run">
+                                                <div class="col-md-2 classGroupInput">
+                                                    <input type="text" class="form-control" placeholder="<?php echo lang('exa_ddmmyy'); ?>" value="<?php echo $startDate; ?>" name="rehearsalDate" readonly="" data-validation="required" data-validation-error-msg="">
+                                                </div>
+                                                <div class="col-md-2 classGroupInput">
+                                                    <select class="form-control" name="day" data-validation="required" data-validation-error-msg="">
+                                                        <option value=""><?php echo lang('exa_sd'); ?></option>
+                                                        <?php foreach ($weeklyDay as $row2) { ?>
+                                                            <option class="<?php echo $row2['status']; ?>" value="<?php echo $row2['day_name']; ?>"><?php echo $row2['day_name']; ?></option>
+                                                        <?php } ?>
+                                                    </select>
+                                                </div>
+                                                <div class="col-md-2 classGroupInput">
+                                                    <select class="form-control" name="song" data-validation="required" data-validation-error-msg="">
+                                                        <option><?php echo lang('exa_ss'); ?></option>
+                                                        <?php foreach ($song as $row1) { ?>
+                                                            <option value="<?php echo $row1['song_title']; ?>"><?php echo $row1['song_title']; ?></option>
+                                                        <?php } ?>
+                                                    </select>
+                                                </div>
+                                                <div class="col-md-2 classGroupInput">
+                                                    <input type="text" class="form-control" placeholder="<?php echo lang('exa_sub_code'); ?>" name="songCode" data-validation="required" data-validation-error-msg="">
+                                                </div>
+                                                <div class="col-md-2 classGroupInput">
+                                                    <input type="text" class="form-control" placeholder="<?php echo lang('exa_rnih'); ?>" name="romeNo" data-validation="required" data-validation-error-msg="">
+                                                </div>
+                                                <div class="col-md-2 classGroupInput">
+                                                    <input type="text" class="form-control" placeholder="<?php echo lang('exa_start_time'); ?>" name="starTima" data-validation="required" data-validation-error-msg="">
+                                                </div>
+                                                <div class="col-md-2 classGroupInput">
+                                                    <input type="text" class="form-control" placeholder="<?php echo lang('exa_end_time'); ?>" name="endTima" data-validation="required" data-validation-error-msg="">
+                                                </div>
+                                                <div class="col-md-2 classGroupInput">
+                                                    <select class="form-control" name="rehearsalShift">
+                                                        <option><?php echo lang('exa_sele_shi'); ?></option>
+                                                        <option><?php echo lang('exa_morn_shi'); ?></option>
+                                                        <option><?php echo lang('exa_even_shi'); ?></option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="addRoutineSongl-md-12">
+                                    <a id="addGroup" class="floatRight btn green">
+                                        <i class="fa fa-plus"></i> <?php echo lang('exa_nexar'); ?>
+                                    </a>
+                                </div><div class="clearfix"> </div>
+                            </div>
+                        </div>
+                        <div class="form-actions fluid">
+                            <div class="col-md-offset-3 col-md-6">
+                                <button type="submit" class="btn green" name="submit" value="Submit"><?php echo lang('save'); ?></button>
+                                <button type="reset" class="btn default"><?php echo lang('refresh'); ?></button>
+                            </div>
+                        </div>
+                        <?php echo form_close(); ?>
+                    </div>
+                </div>
+                <!-- END SAMPLE FORM PORTLET-->
+            </div>
+        </div>
+        <!-- END PAGE CONTENT-->
+    </div>
+</div>
+<!-- END CONTENT -->
+<script src="assets/global/plugins/jquery.form-validator.min.js" type="text/javascript"></script>
+<script> $.validate();</script>
+<script>
+    $(function () {
+        var maxFild = 15;
+        var scntDiv = $('#div_scents');
+        var i = $('#div_scents').size() + 1;
+
+        var x = 1;
+        $('#addGroup').live('click', function () {
+            if (x < maxFild) {
+                x++;
+                $('<div id="remove" class="ClassGroupInput" ><hr><h3 class="arpl"><?php echo lang('exa_rehearsal'); ?> ' + i + '</h3><input type="hidden" class="form-control" name="rehearsalSunjectFild_' + i + '" value="run"><div class="row"><div class="col-md-12"><div class="col-md-2 classGroupInput"><input type="text" class="form-control" placeholder="<?php echo lang('exa_ddmmyy'); ?>" name="rehearsalDate_' + i + '" data-validation="required" data-validation-error-msg=""></div>\n\<div class="col-md-2 classGroupInput"><select class="form-control" name="day_' + i + '"><option><?php echo lang('exa_sd'); ?></option><?php foreach ($weeklyDay as $row2) { ?><option class="<?php echo $row2['status']; ?>"  value="<?php echo $row2["day_name"]; ?>"><?php echo $row2["day_name"]; ?></option><?php } ?></select></div><div class="col-md-2 classGroupInput"><select class="form-control" name="song_' + i + '"><option><?php echo lang('exa_ss'); ?></option><?php foreach ($song as $row1) { ?><option value="<?php echo $row1["song_title"]; ?>"><?php echo $row1["songtle"]; ?></option><?php } ?></select></div><div class="col-md-2 classGroupInput"><input type="text" class="form-control" placeholder="<?php echo lang('exa_sub_code'); ?>" name="songCode_' + i + '" data-validation="required" data-validation-error-msg=""></div><div class="col-md-2 classGroupInput"><input type="text" class="form-control" placeholder="<?php echo lang('exa_rnih'); ?>" name="romeNo_' + i + '" data-validation="required" data-validation-error-msg=""></div><div class="col-md-2 classGroupInput"><input type="text" class="form-control" placeholder="<?php echo lang('exa_start_time'); ?>" name="starTima_' + i + '" data-validation="required" data-validation-error-msg=""></div><div class="col-md-2 classGroupInput"><input type="text" class="form-control" placeholder="<?php echo lang('exa_end_time'); ?>" name="endTima_' + i + '" data-validation="required" data-validation-error-msg=""></div><div class="col-md-2 classGroupInput"><select class="form-control" name="rehearsalShift_' + i + '"><option><?php echo lang('exa_sele_shi'); ?></option><option><?php echo lang('exa_morn_shi'); ?></option><option><?php echo lang('exa_even_shi'); ?></option></select></div></div></div><a href="#" id="remGroup" class="arplremove">Remove</a></div>').appendTo(scntDiv);
+                i++;
+                return false;
+            }
+        });
+
+        $('#remGroup').live('click', function () {
+            if (i > 2) {
+                $(this).parents('#remove').remove();
+                i--;
+                x--;
+            }
+            return false;
+        });
+    });
+</script>
---
+++ .history/ISFM/modules/songs/views/addSong_20240506131332.php
@@ -0,0 +1,119 @@
+
+<!-- BEGIN CONTENT -->
+<div class="page-content-wrapper">
+    <div class="page-content">
+        <!-- BEGIN PAGE HEADER-->
+        <div class="row">
+            <div class="col-md-12">
+                <!-- BEGIN PAGE TITLE & BREADCRUMB-->
+                <h3 class="page-title">
+                    <?php echo lang('sub_asfc'); ?> <small></small>
+                </h3>
+                <ul class="page-breadcrumb breadcrumb">
+                    <li>
+                        <i class="fa fa-home"></i><?php echo lang('home'); ?>
+                        
+                    </li>
+                    <li>
+                        <?php echo lang('sub_song'); ?>
+                        
+                    </li>
+                    <li>
+                        <?php echo lang('sub_add_song'); ?>
+                    </li>
+                    <li id="result" class="pull-right topClock"></li>
+                </ul>
+                <!-- END PAGE TITLE & BREADCRUMB-->
+            </div>
+        </div>
+        <!-- END PAGE HEADER-->
+        <!-- BEGIN PAGE CONTENT-->
+        <div class="row">
+            <div class="col-md-12 ">
+                <?php if (!empty($success)) {
+                    echo $success;
+                } ?>
+                <!-- BEGIN SAMPLE FORM PORTLET-->
+                <div class="portlet box purple ">
+                    <div class="portlet-title">
+                        <div class="caption">
+                            <i class="fa fa-Resource"></i> <?php echo lang('sub_add_new_subj'); ?>
+                        </div>
+                        <div class="tools">
+                            <a href="" class="collapse">
+                            </a>
+                            <a href="" class="reload">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="portlet-body form">
+                        <?php $form_attributs = array('class' => 'form-horizontal', 'role' => 'form');
+                        echo form_open('songs/addSong', $form_attributs);
+                        ?>
+                            <div class="form-body">
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label"> <?php echo lang('Choir'); ?> <span class="requiredStar"> * </span></label>
+                                    <div class="col-md-6">
+                                        <select class="form-control" name="Choir_id" data-validation="required" data-validation-error-msg="">
+                                            <option value=""><?php echo lang('select'); ?></option>
+                                            <?php foreach ($Choir as $row) { ?>
+                                                <option value="<?php echo $row['id']; ?>"><?php echo $row['Choir_title']; ?></option>
+<?php } ?>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label"> <?php echo lang('sub_title'); ?> <span class="requiredStar">  </span></label>
+                                    <div class="col-md-6">
+                                        <input type="text" name="song" class="form-control" placeholder="Song title here" data-validation="required" data-validation-error-msg="">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label"> <?php echo lang('sub_edition'); ?> <span class="requiredStar">  </span></label>
+                                    <div class="col-md-6">
+                                        <input type="text" name="edition" class="form-control" placeholder="Song Edition here">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label"> <?php echo lang('sub_aut_name'); ?> <span class="requiredStar">  </span></label>
+                                    <div class="col-md-6">
+                                        <input type="text" name="writer_name" class="form-control" placeholder="Song Authore Name here">
+                                    </div>
+                                </div>
+                                <!-- <div class="form-group">
+                                        <label class="col-md-3 control-label"> <?php echo lang('sub_opt_subj'); ?> </label>
+                                        <div class="radio-list">
+                                                <label class="radio-inline">
+                                                <input type="radio" name="optionalSong" id="optionsRadios4" value="1"> Yes </label>
+                                                <label class="radio-inline">
+                                                <input type="radio" name="optionalSong" id="optionsRadios5" value="0" checked> No </label>
+                                        </div>
+                                </div> -->
+                            </div>
+                            <div class="form-actions fluid">
+                                <div class="col-md-offset-3 col-md-6">
+                                    <button type="submit" name="submit" class="btn green" value="Add Song"> <?php echo lang('sub_addsub_but'); ?> </button>
+                                    <button type="reset" class="btn default"> <?php echo lang('cancel'); ?> </button>
+                                </div>
+                            </div>
+                        <?php echo form_close(); ?>
+                    </div>
+                </div>
+                <!-- END SAMPLE FORM PORTLET-->
+            </div>
+        </div>
+        <!-- END PAGE CONTENT-->
+    </div>
+</div>
+<!-- END CONTENT -->
+<script>
+    jQuery(document).ready(function() {
+//here is auto reload after 1 second for time and date in the top
+        jQuery(setInterval(function() {
+            jQuery("#result").load("index.php/home/iceTime");
+        }, 1000));
+    });
+</script>
+<script src="assets/global/plugins/jquery.form-validator.min.js" type="text/javascript"></script>
+<script> $.validate(); </script>
